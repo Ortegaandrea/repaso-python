f5d3847f9bce737b2bb36bd759fa66c2ab386104
Andrea <al.andreaortega@iesramonllull.net>
2023-10-27 12:52:39 +0200

    mi primer commit

---
+++ main.py
@@ -0,0 +1,56 @@
+print("Hola mundo")
+print("Hola mundo 2")
+
+# CVomentario
+print("Hola mundo 3")
+
+"""
+texto que no se va a interpretar
+texto que no se va a interpretar
+texto que no se va a interpretar
+"""
+
+#Variables
+texto = "repaso"
+nombre = "andrea"
+altura= "167"
+year= 2006
+
+print(texto + " - " + nombre + " - " + altura + " - " + str(year))
+
+#Entrada
+#sitioweb= input("¿Cuál es tu página web?: ")
+#print=("El sitio web del usuario es:  " + sitioweb)
+
+#Condiciones
+"""
+altura= int(input("¿Cuál es tu altura?: "))
+
+if altura >= 180:
+    print("Eres una persona alta!")  
+else: 
+    print("eres bajito")
+"""
+
+# Funciones 
+"""
+var_altura= int(input("¿Cuál es tu altura?: "))
+
+def mostrarAltura(altura):
+    resultado = ""
+    if altura >= 180:
+        resultado = "Eres una persona alta!"  
+    else: 
+        resultado = "eres bajito"
+    
+    return resultado
+
+print(mostrarAltura(var_altura))
+"""
+# Listas
+personas = ¨{"pepe", "pepita", "pepote"} 
+
+print(personas {2})
+
+for persona in personas: 
+    print("." + persona) 
